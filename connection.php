40f47738dd09f0c0412fdc3dfe5519ee875df2f9
Samanthapuri <33673268+Samanthapuri@users.noreply.github.com>
2019-02-03 19:20:40 +0530

    commit- 7

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-$db = mysqli_connect("mysql", "rakesh", "legendary", "login") or die ("Failed to connect");
+$db = mysqli_connect("mysql", "rakesh", "Welcome@123", "phpdb") or die ("Failed to connect");
 
 ?>
